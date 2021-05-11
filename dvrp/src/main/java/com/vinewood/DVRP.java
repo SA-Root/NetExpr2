1fb85d09a2c848f9c330aa93b4ff92c29b5af426
SA-Root <a124000@outlook.com>
2021-05-11 17:18:11 +0800

    first commit

---
+++ dvrp/src/main/java/com/vinewood/DVRP.java
@@ -0,0 +1,13 @@
+package com.vinewood;
+
+/**
+ * Hello world!
+ *
+ */
+public class DVRP 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
