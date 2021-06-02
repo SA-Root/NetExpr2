d1a0b49306cbc03eb658370b0de276df050017c3
SA-Root <a124000@outlook.com>
2021-06-02 17:54:53 +0800

    trim

--- dvrp/src/main/java/com/vinewood/RouterInstance.java
+++ dvrp/src/main/java/com/vinewood/RouterInstance.java
@@ -57,39 +57,32 @@ public class RouterInstance {
             }
             if (!QueueExchangeReceived.isEmpty()) {
                 var getSize = QueueExchangeReceived.size();
-                if (!isRunning) {
-                    while (getSize-- > 0) {
-                        QueueExchangeReceived.poll();
-                    }
-                } else {
-                    while (getSize-- > 0) {
-                        RouterExchange[] exchangesNow = QueueExchangeReceived.poll();
+                while (getSize-- > 0) {
+                    RouterExchange[] exchangesNow = QueueExchangeReceived.poll();
 
-                        // HashMap<String,RoutingInfo> rtNow=new
-                        // HashMap<String,RoutingInfo>(RoutingTable);
-                        for (RoutingInfo it : RoutingTable.values()) {
-                            if (!it.DestNode.equals(it.Neighbour))
-                                it.Distance = Config.Unreachable;
-                        }
-
-                        for (RouterExchange exchangeNow : exchangesNow) {
-                            var destInfo = RoutingTable.get(exchangeNow.DestNode);
-                            if (destInfo.Distance > RoutingTable.get(exchangeNow.SrcNode).Distance
-                                    + exchangeNow.Distance) {
-                                destInfo.Neighbour = exchangeNow.SrcNode;
-                                destInfo.Distance = exchangeNow.Distance;
-                            }
-                        }
-                        
-                        sendMsgOnce();
+                    // HashMap<String,RoutingInfo> rtNow=new
+                    // HashMap<String,RoutingInfo>(RoutingTable);
+                    for (RoutingInfo it : RoutingTable.values()) {
+                        if (!it.DestNode.equals(it.Neighbour))
+                            it.Distance = Config.Unreachable;
                     }
-                   
+
+                    for (RouterExchange exchangeNow : exchangesNow) {
+                        var destInfo = RoutingTable.get(exchangeNow.DestNode);
+                        if (destInfo.Distance > RoutingTable.get(exchangeNow.SrcNode).Distance + exchangeNow.Distance) {
+                            destInfo.Neighbour = exchangeNow.SrcNode;
+                            destInfo.Distance = exchangeNow.Distance;
+                        }
+                    }
+
+                    sendMsgOnce();
                 }
             }
         }
         QueueExchangeReceived.clear();
     }
-    public void sendMsgOnce(){
+
+    public void sendMsgOnce() {
         var exchanges = new ArrayList<RouterExchange>();
         for (RoutingInfo it : RoutingTable.values()) {
             exchanges.add(new RouterExchange(LocalID, it.DestNode, it.Distance));
@@ -119,6 +112,7 @@ public class RouterInstance {
         }
         PrintRoutingInfo();
     }
+
     public RouterInstance(String id, int udpport, String ifpath) {
         RoutingTable = new HashMap<String, RoutingInfo>();
         NeighbourMap = new HashMap<String, Integer>();
@@ -222,7 +216,6 @@ public class RouterInstance {
         TUDPListener.start();
         InitializeNode();
         System.out.println("[INFO]Router is running.");
-        sendMsgOnce();
         while (true) {
             var command = ' ';
             try {
@@ -343,6 +336,6 @@ public class RouterInstance {
         NeighbourAlive.clear();
         NeighbourMap.clear();
         LoadConfig(configPath);
-        // TODO: impl
+        sendMsgOnce();
     }
 }
